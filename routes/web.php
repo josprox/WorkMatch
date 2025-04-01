2d0a67dffd7346a09604557d9323478b403bb1e1
josprox <joss@int.josprox.com>
2025-04-01 16:16:49 -0600

    Levantamos API y configuramos bloqueo auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+// Página de inicio
+Route::get('/', function () {
+    return view('auth.login'); // Cambia 'welcome' por 'login'
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +14,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/register', function () {
+    return redirect('/'); // Redirige a la página de login
+});
 
