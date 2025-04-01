63f5c86efb8b83eee3494be49c5e449da62dc22c
josprox <joss@int.josprox.com>
2025-04-01 16:08:58 -0600

    Cambios del diseño e integración de Tablar

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,69 @@
+@extends('tablar::auth.layout')
+@section('title', 'Inicia sesión')
+@section('content')
+    <div class="container container-tight py-4">
+        <div class="text-center mb-1 mt-5">
+            <a href="" class="navbar-brand navbar-brand-autodark">
+                <img src="{{asset(config('tablar.auth_logo.img.path','assets/logo.svg'))}}" height="36"
+                     alt=""></a>
+        </div>
+        <div class="card card-md">
+            <div class="card-body">
+                <h2 class="h2 text-center mb-4">Accede con tu cuenta</h2>
+                <form action="{{route('login')}}" method="post" autocomplete="off" novalidate>
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Correo electrónico</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                               placeholder="your@email.com"
+                               autocomplete="off">
+                        @error('email')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-label">
+                            Contraseña
+                            <span class="form-label-description">
+                    <a href="{{route('password.request')}}">Olvidé la contraseña</a>
+                  </span>
+                        </label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" name="password"
+                                   class="form-control @error('password') is-invalid @enderror"
+                                   placeholder="Your password"
+                                   autocomplete="off">
+                            <span class="input-group-text">
+                    <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12"
+                                                                                                              cy="12"
+                                                                                                              r="2"/><path
+                              d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"/></svg>
+                    </a>
+                  </span>
+                            @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-check">
+                            <input type="checkbox" class="form-check-input"/>
+                            <span class="form-check-label">Recordame en este dispositivo</span>
+                        </label>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        @if(Route::has('register'))
+            <div class="text-center text-muted mt-3">
+                Don't have account yet? <a href="{{route('register')}}" tabindex="-1">Regístrate</a>
+            </div>
+        @endif
+    </div>
+@endsection
