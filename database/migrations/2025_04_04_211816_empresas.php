f3ac0e714862283c54237e1659a95197dc6a3a92
josprox <joss@int.josprox.com>
2025-04-05 16:22:41 -0600

    Cambios en las tablas, mejor adaptación

--- database/migrations/2025_04_04_211816_empresas.php
+++ database/migrations/2025_04_04_211816_empresas.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
             $table->id(); // id BIGINT auto_increment
-            $table->string('nombre', 100);
-            $table->string('correo', 100)->unique();
-            $table->string('ubicacion', 100)->nullable();
-            $table->string('telefono', 20)->nullable();
+            $table->string('nombre', 100)->comment('Nombre de la empresa');
+            $table->string('correo', 100)->unique()->comment('Correo electrónico del cliente');
+            $table->string('contra', 255)->comment('Contraseña encriptada del cliente');
+            $table->string('ubicacion', 100)->nullable()->comment('Ubicación de la empresa');
+            $table->string('telefono', 20)->nullable()->comment('Teléfono de la empresa');
             $table->timestamps(); // created_at, updated_at
         });
     }
